f010204dc3ba231dcceb49fa95d210e4e2723d39
-Trecy-Malonza <trecymalonza@gmail.com>
2024-02-08 20:42:03 +0300

    added file

---
+++ src/Main.kt
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+ var fullname: String = "Trecy Malonza"
+ var age: Byte = 19
+ var phonenumber: String = "+254700461482"
+ var kg: Float = 63.5F
+ var kenyan = true
+
+
+}
+
